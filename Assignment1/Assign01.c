f6e7eb65de593d38c51ee2f527d4f0481951256b
rubeenaafreen20 <70516674+rubeenaafreen20@users.noreply.github.com>
2022-01-16 02:39:49 +0530

    Add files via upload

--- Assignment1/Assign01.c
+++ Assignment1/Assign01.c
@@ -1,25 +1,38 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
+#include <stdbool.h>
+
+// function for NAND operation
+bool NAND(bool x,bool y){
+    bool z=!(x && y);
+    return z;
+}
 unsigned char sop(A,B,C)
 {
     unsigned char F;
-    F = ((~B)&(~C))|(B&C);
+    F = (B&C)|((!B)&(!C));
     return F;
 }
 int main(){
+    unsigned char X;
     unsigned char G; //output
     unsigned char one = 0x01;//used for displaying the output in bit
-    unsigned char  A=0x00,B=0x00,C=0x01; //inputs in hex
+    unsigned char  A=0x00,B=0x01,C=0x00; //inputs in hex
     G=sop(A,B,C);
     printf("%x%x%x",one&A,one&B,one&C);
     printf(" ");
     printf("%x\n" ,one&G);
-    return 0;
+  //Implementation of POS using NAND gate
+    X=NAND(NAND(B,C),NAND((!B),(!C)));
+   printf("OUTPUT:%d",X);
+
+return 0;
+
 }
